dbe79f2f81e42ce5d6ebd713a6f44b92884657b4
Rakesh Padigela <rakesh.p2906@gmail.com>
2020-04-16 18:51:35 -0500

    Rewards test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import data from './data';
 
 function App() {
+  const [loadedData, setloadedData] = useState({});
+  const [userRewards, setCalcRewards] = useState({});
+  const [userTransactions, setUserTransactions] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState("");
+
+
+  useEffect(() => {
+    console.log('Help');
+    setloadedData({ ...data });
+    setUsers([...Object.keys(data)]);
+
+  }, []);
+
+
+  const userSelect = (value) => {
+    setCurrentUser(value);
+    let userData = loadedData[value];
+
+    let monthT = {
+      1: {
+        amounts: [],
+        rewards: 0,
+      },
+      2: {
+        amounts: [],
+        rewards: 0,
+      },
+      3: {
+        amounts: [],
+        rewards: 0,
+      },
+    };
+    for (let i = 0; i < userData.length; i++) {
+      let month = new Date(userData[i]['date']);
+      if (month.getMonth() + 1 === 1 || month.getMonth() + 1 === 2 || month.getMonth() + 1 === 3) {
+        monthT[month.getMonth() + 1]['amounts'].push(userData[i]['amount']);
+      }
+    }
+    for (let key in monthT) {
+      let total_month_rewards = 0;
+      for (let i = 0; i < monthT[key]['amounts'].length; i++) {
+        let price = monthT[key]['amounts'][i];
+
+        total_month_rewards = total_month_rewards + calRew(price);
+      }
+      monthT[key]['rewards'] = total_month_rewards;
+    }
+    console.log(monthT)
+    setCalcRewards({ ...monthT });
+    setUserTransactions([...userData]);
+  };
+
+
+
+  function calRew(price) {
+    let rewards = 0;
+    if (price > 100) {
+      rewards = (price - 100) * 2;
+    }
+    if (price > 50) {
+      rewards = rewards + (price - 50);
+    }
+    return rewards;
+  
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //username 8characters
+    <div className="container bg-white">
+    <div className="row">
+    <div className="col-md-12">
+      <div className="text-center h2 m-4 ">Rewards Dashbaord</div>
+  
+      <div className="select-style offset-md-4 ">
+        <select className="dropdown dropdown-item col-md-4 border border-info m-5" onChange={e => userSelect(e.target.value)} value={currentUser} >
+          <option value="" disabled>Select User</option>
+          {users.map((item, index) => {
+            return (
+              <option key={index} value={item}> {item.toUpperCase()} </option>
+            );
+          })}
+        </select>
+      </div>
+      {Object.keys(userRewards).length > 0 &&
+        <div>
+          <table className="table table-info">
+            <thead>
+              <tr>
+                <th>Month</th>
+                <th>Rewards</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>First Month</td>
+                <td>{userRewards[1]["rewards"]}</td>
+              </tr>
+              <tr>
+                <td>Second Month</td>
+                <td>{userRewards[2]["rewards"]}</td>
+              </tr>
+              <tr>
+                <td>Third Month</td>
+                <td>{userRewards[3]["rewards"]}</td>
+              </tr>
+      
+              <tr>
+                <td><strong>Total Reward</strong></td>
+                <td><strong>{userRewards[1]["rewards"] + userRewards[2]["rewards"] + userRewards[3]["rewards"]}</strong></td>
+              </tr>
+              
+            </tbody>
+          </table>
+          <h2 className="text-center h3 m-4">{}Transactions</h2>
+          {userTransactions.length > 0 ?
+            <table className="table table-info">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Amount</th>
+                  <th>Rewards</th>
+                </tr>
+
+              </thead>
+              <tbody>
+                {userTransactions.map((item, index) => {
+                  return <tr key={index}>
+                    <td>{item["date"]}</td>
+                    <td>{item["amount"]}</td>
+                    <td>{calRew(item["amount"])}</td>
+                  </tr>
+                })}
+              </tbody>
+            </table>
+            : <div>No Transactions Found</div>}
+          
+        </div>
+      }
+      </div>
+      </div>
+    </ div >
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,124 @@
+module.exports = {
+    Kristien: [
+        {
+          date: "1/1/2020",
+          amount: 100,
+          
+        },
+        {
+          date: "2/1/2020",
+          amount: 10
+        },
+        {
+          date: "3/1/2020",
+          amount: 200
+        },
+        {
+          date: "1/1/2020",
+          amount: 650
+        },
+        {
+          date: "1/1/2020",
+          amount: 110
+        },
+        {
+          date: "3/1/2020",
+          amount: 500
+        },
+        {
+          date: "2/1/2020",
+          amount: 170
+        },
+        {
+          date: "3/1/2020",
+          amount: 120
+        }
+      ],
+      Cirillo: [
+        {
+          date: "1/1/2020",
+          amount: 100
+        },
+        {
+          date: "2/28/2020",
+          amount: 140
+        },
+        {
+          date: "3/15/2020",
+          amount: 1300
+        },
+        {
+          date: "2/11/2020",
+          amount: 256
+        },
+        {
+          date: "3/24/2020",
+          amount: 35
+        },
+        {
+          date: "2/2/2020",
+          amount: 115
+        }
+      ],
+      Farlay: [
+        {
+          date: "2/1/2020",
+          amount: 2354
+        },
+        {
+          date: "1/26/2020",
+          amount: 120
+        },
+        {
+          date: "3/4/2020",
+          amount: 114
+        },
+        {
+          date: "3/24/2020",
+          amount: 345
+        },
+        {
+          date: "1/2/2020",
+          amount: 2389
+        },
+        {
+          date: "1/12/2020",
+          amount: 1456
+        },
+        {
+          date: "2/12/2020",
+          amount: 38
+        }
+      ],
+      Gabbi: [
+          {
+              date: "2/1/2020",
+              amount: 40
+            },
+            {
+              date: "1/26/2020",
+              amount: 0
+            },
+            {
+              date: "3/4/2020",
+              amount: 11
+            },
+            {
+              date: "3/24/2020",
+              amount: 3
+            },
+            {
+              date: "1/2/2020",
+              amount: 29
+            },
+            {
+              date: "1/12/2020",
+              amount: 1456
+            },
+            {
+              date: "2/12/2020",
+              amount: 38
+            }
+      ]
+    };
+  
